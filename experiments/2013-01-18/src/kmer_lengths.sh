57cc62ec89eab9d194a503f0a86113682a1c91d9
Johannes Alneberg <alneberg@kth.se>
2013-01-21 11:16:41 +0100

    Some documentation on experiment kmer lengths

--- experiments/2013-01-18/src/kmer_lengths.sh
+++ experiments/2013-01-18/src/kmer_lengths.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-echo "Sampling contigs"
+# This experiment aim is to compare how different kmer-lengths affect 
+# the genomic profile specificity for different taxonomic levels.
+echo "Sampling contigs uniformly from reference genomes"
 ./contig_generation.py ~/Dropbox/Shared/Binni_Johannes/references.fa -v -n 100 --max_length 10000 --min_length 30 -o ../results/generated_contigs.fa
-echo "Continue with probin"
+# Each contig should be scored against each genome. 
+echo "Fitting the multinomial parameters to each genome"
 ../../../src/ProBin.py ../results/generated_contigs.fa -o ../results/log_prob_for_contigs.fa
+echo "Scoring contigs against its proper genome"
+echo "Scoring contigs against all other genomes"
