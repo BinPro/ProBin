e142954058d0a9dd749419436e041e26731c3ea9
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-02-07 02:10:41 +0100

    Rename since usage can be other than just contigs

--- src/probin/dna.py
+++ src/probin/dna.py
@@ -1,6 +1,6 @@
 from itertools import product
 from collections import Counter
-class Contig(object):
+class DNA(object):
     BASE_COMPLEMENT = {"A":"T","T":"A","G":"C","C":"G"}
     kmer_hash={}
     kmer_len = None
@@ -20,24 +20,25 @@ class Contig(object):
     
     def __init__(self,id,seq):
         if not self.kmer_len:
-            raise Exception("Please run Contig.generate_kmer_hash(kmer_len) first.")
+            raise Exception("Please run DNA.generate_kmer_hash(kmer_len) first.")
         self.id = id
         self.seq = seq.upper().split("N")
         self.signature = self.calculate_signature()
         
     def calculate_signature(self):
+        signature = Counter()
         for fragment in self.seq:
             if len(fragment) < self.kmer_len:
                 continue
             indexes = [self.kmer_hash[fragment[i:i+self.kmer_len]] for i in xrange(len(fragment) - (self.kmer_len-1))]
-            signature = Counter(indexes)
+            signature.update(indexes)
         return signature
 
 if __name__=="__main__":
-    Contig.generate_kmer_hash(4)
-    a = Contig(id="ADF",seq="ACTTTAAACCCACACACAACATTTGGGGAGAGAGCCATTA")
+    DNA.generate_kmer_hash(4)
+    a = DNA(id="ADF",seq="ACTTNACTT")
     print a.signature
-    b = Contig(id="ADFA",seq="ACTTTAAACCCACACACAACATTTGGAAAGGAGAGAGCCATTA")
+    b = DNA(id="ADFA",seq="ACTTTAAACCCACACACAACATTTGGAAAGGAGAGAGCCATTA")
     print b.signature
-    c = Contig(id="ADADAD",seq='AAAATTTTACGTAGAGCCATTGAGACCTT')
+    c = DNA(id="ADADAD",seq='AAAATTTTACGTAGAGCCATTGAGACCTT')
     print c.signature
