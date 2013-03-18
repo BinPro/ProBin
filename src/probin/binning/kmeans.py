f2f3f76c654142f301ac882ff11628faace11ff4
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-03-18 14:54:47 +0100

    K-means working for test of two contigs from two genomes, so four contigs in total

--- src/probin/binning/kmeans.py
+++ src/probin/binning/kmeans.py
@@ -2,103 +2,37 @@
 """Cluster DNA based on k-means algorithm with fixed number of clusters"""
 from probin.dna import DNA
 import numpy as np
-from numpy.random import randint
 from collections import Counter
 
-def cluster(contigs, cluster_count,model):
-    centroids = _generate_centroids(contigs,cluster_count,model)
-    
-    clusters = [set() for i in xrange(cluster_count)]
-    
-    keep_clustering = True
-    counter = 0
-    while keep_clustering:
-        keep_clustering=False
-        clusters_new = _expectation(contigs,centroids,model)
-        centroids = _maximization(clusters,model)
-        number_same = 0
-        for cluster in clusters_new:
-            if cluster not in clusters:
-                keep_clustering = True
-            else:
-                number_same += 1
-            if cluster == set():
-                print "empty"
-        (clusters, clusters_new) = (clusters_new,clusters)
-        counter += 1
-        print number_same
-        print counter
-        print "-"*70
-    return clusters
-    
-def _generate_centroids(contigs,cluster_count ,model):
-    centroids = np.zeros((cluster_count,DNA.kmer_hash_count))
-    centroid_contigs = set()
-    first_centroid_contig = randint(low=0,high=cluster_count)
-    centroid_contigs.add(first_centroid_contig)
-    print first_centroid_contig
-    centroids[0,:] = model.fit_nonzero_parameters(contigs[first_centroid_contig].signature,DNA.kmer_hash_count)
-    for ind_next_centroid in xrange(1,cluster_count):
-        dist = np.zeros(len(contigs))
-        for ind_contig,contig in enumerate(contigs):
-            for ind_done_centroid in xrange(ind_next_centroid):
-                dist[ind_contig] = min(dist[ind_contig],model.log_probability(contig.signature,centroids[ind_done_centroid]))
-        dist = np.power(dist,2)
-        dist /= np.sum(dist)
-        while True:
-            next_centroid_contig = np.argmax(np.random.random(dist.shape) * dist)
-            if next_centroid_contig not in centroid_contigs:
-                centroid_contigs.add(next_centroid_contig)
-                break
-            print "had to find another"
-        print next_centroid_contig
-        centroids[ind_next_centroid,:] = model.fit_nonzero_parameters(contigs[next_centroid_contig].signature,DNA.kmer_hash_count)
-    print "done centroids"
+def cluster(contigs, cluster_count,model,centroids=None):
+    if centroids is None:
+       centroids = _generate_centroids(cluster_count,DNA.kmer_hash_count)
+    cluster_different = True
+    while (cluster_different):
+        clusters = [set() for _ in xrange(cluster_count)]
+
+        #Expectations
+        for contig in contigs:
+            prob = [model.log_probability(contig.signature,centroid) for centroid in centroids]
+            clust_ind = np.argmax(prob)
+            clusters[clust_ind].add(contig)
+        
+        #Maximization
+        new_centroids = np.zeros(centroids.shape)
+        for clust_ind ,clust in enumerate(clusters):
+            new_centroid = Counter()
+            [new_centroid.update(contig.signature) for contig in clust]
+            new_centroids[clust_ind,:] = model.fit_nonzero_parameters(new_centroid,DNA.kmer_hash_count)
+        if (new_centroids == centroids).all():
+            cluster_different = False
+        (new_centroids,centroids) = (centroids,new_centroids)
     return centroids
     
-def _expectation(contigs,centroids,model):
-    clusters_new = [set() for i in xrange(len(centroids))]
-    for i,contig in enumerate(contigs):
-        max_probability = None
-        max_clusters = []        
-        for centroid_index,centroid in enumerate(centroids):
-            centroid_probability = model.log_probability(contig.signature,centroid)
+def _generate_centroids(c_count,c_dim):
+    centroids = np.random.rand(c_count,c_dim)
+    centroids /= np.sum(centroids,axis=1,keepdims=True)
+    return centroids
 
-            if centroid_probability >= max_probability:
-                if centroid_probability > max_probability:
-                    max_clusters = []
-                max_probability = centroid_probability
-                max_clusters.append(centroid_index)
-        clusters_new[max_clusters[0]].add(contig)
-    return clusters_new
-    
-def _maximization(clusters,model):
-    centroids_new = np.zeros((len(clusters),DNA.kmer_hash_count))
-    for i,cluster in enumerate(clusters):
-        if not len(cluster):
-            c = np.random.rand(DNA.kmer_hash_count)
-            c /= np.sum(c)
-            centroids_new[i,:] = c
-        else:        
-            clust_sig = Counter()
-            [clust_sig.update(contig.signature) for contig in cluster]
-            centroids_new[i,:] = model.fit_nonzero_parameters(clust_sig,DNA.kmer_hash_count)
-    return centroids_new
 
-if __name__=="__main__":
-    from Bio import SeqIO
-    from probin.model.composition import multinomial
-    FASTA="../../../tests/generated_contigs_test.fna"
-    DNA.generate_kmer_hash(4)
-    cluster_count = 2
-    with open(FASTA,"r") as fh:
-        seqs = SeqIO.parse(fh,"fasta")
-        seqs = list(seqs)
-    contigs = []
 
-    for seq in seqs:
-        contigs.append(DNA(seq.id,seq.seq.tostring()))
-    for contig in contigs:
-        contig.calculate_signature()
-    clusters = cluster(contigs,7,multinomial)
 
--- tests/test_k-means.py
+++ tests/test_k-means.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from nose.tools import assert_almost_equal, assert_equal
+from nose.tools import assert_almost_equal, assert_equal, assert_true
 from probin import dna
 from probin.binning import kmeans
 from Bio import SeqIO
@@ -9,27 +9,23 @@ import numpy as np
 import sys
 
 class TestKmeans(object):
-    FASTA=""">genome1
-GGGGCCCCTTTTTAAAATTATATGCGCGCGACAACACTG
->genome2
-ATTATATATGAGAGATACGCGCGCTGTGTCTCTGCTGC
->genome3
-GGGGCCCCTTTTTAAAATTATATGCGCGCGCAACACGG
->genome4
-ATTATATATGAGAGCGCGCGCGGTGTGTCTCTGCTGC
-"""
+    FASTA="tests/generated_contigs_test.fna"
     def setUp(self):
         dna.DNA.generate_kmer_hash(4)
         self.cluster_count = 2
-        with tempfile.NamedTemporaryFile() as fh:
-            fh.write(self.FASTA)
-            fh.seek(0)
+        with open(self.FASTA,"r") as fh:
             seqs = SeqIO.parse(fh,"fasta")
             seqs = list(seqs)
         self.contigs = []
 
         for seq in seqs:
-            self.contigs.append(dna.DNA(seq.id,seq.seq.tostring()))
+            if  seq.id.startswith("Ehrlichia_canis_Jake_uid58071_1000") or \
+                seq.id.startswith("Ehrlichia_canis_Jake_uid58071_1001") or \
+                seq.id.startswith("Ehrlichia_ruminantium_Welgevonden_uid58243_1003") or \
+                seq.id.startswith("Ehrlichia_ruminantium_Welgevonden_uid58243_1004"):
+
+                self.contigs.append(dna.DNA(seq.id,seq.seq.tostring()))
+            
         for contig in self.contigs:
             contig.calculate_signature()
             
@@ -37,21 +33,36 @@ ATTATATATGAGAGCGCGCGCGGTGTGTCTCTGCTGC
     def tearDown(self):
         reload(dna)
         self.contigs = []
+
+    def test_contigs_created(self):
+        c_ids = set(["Ehrlichia_canis_Jake_uid58071_1000", "Ehrlichia_canis_Jake_uid58071_1001","Ehrlichia_ruminantium_Welgevonden_uid58243_1003", "Ehrlichia_ruminantium_Welgevonden_uid58243_1004"])
+        for contig in self.contigs:
+            assert_true(contig.id in c_ids)
+            
+    def test_generate_centroids(self):
+        centroids = kmeans._generate_centroids(2,5)
+        assert_equal(len(centroids), 2)
+        assert_equal(len(centroids[0]),5 )
+        assert_equal(np.sum(centroids,axis=1).all(),1)
         
-    def test_simple_binning(self):
-        clusters = kmeans.cluster(self.contigs,self.cluster_count,multinomial)
-        s = set()
-        [set.add(x) for x in clusters]
-        assert_equal(len(self.contigs),len(s)) 
-        
-    def test_expectation(self):
-        centroids = np.zeros((self.cluster_count,dna.DNA.kmer_hash_count))
+    def test_cluster_perfect_center(self):
+        centroids = np.zeros((2,dna.DNA.kmer_hash_count))
+        centroids[0,:] = multinomial.fit_nonzero_parameters(self.contigs[0].signature + self.contigs[1].signature,dna.DNA.kmer_hash_count)
+        centroids[1,:] = multinomial.fit_nonzero_parameters(self.contigs[2].signature + self.contigs[3].signature,dna.DNA.kmer_hash_count)
+        new_centroids = kmeans.cluster(self.contigs,2,multinomial,centroids)
+        assert_equal((centroids==new_centroids).all(),True)
+
+    def test_cluster_semi_center(self):
+        centroids = np.zeros((2,dna.DNA.kmer_hash_count))
         centroids[0,:] = multinomial.fit_nonzero_parameters(self.contigs[0].signature,dna.DNA.kmer_hash_count)
         centroids[1,:] = multinomial.fit_nonzero_parameters(self.contigs[2].signature,dna.DNA.kmer_hash_count)
-        kmeans._expectation(self.contigs,centroids,multinomial)
+        new_centroids = kmeans.cluster(self.contigs,2,multinomial,centroids)
+
+        correct_centroids = np.zeros((2,dna.DNA.kmer_hash_count))
+        correct_centroids[0,:] = multinomial.fit_nonzero_parameters(self.contigs[0].signature + self.contigs[1].signature,dna.DNA.kmer_hash_count)
+        correct_centroids[1,:] = multinomial.fit_nonzero_parameters(self.contigs[2].signature + self.contigs[3].signature,dna.DNA.kmer_hash_count)        
+
+        assert_equal((correct_centroids==new_centroids).all(),True)
         
-    
-    
-    def test_maximization(self):
-        pass
-    
+        
+        
