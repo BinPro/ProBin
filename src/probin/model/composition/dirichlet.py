083d3f33656fa35ce7293f4c8f81593cbfabdf2a
Johannes Alneberg <alneberg@kth.se>
2013-03-19 12:17:31 +0100

    Dirichlet density function

--- src/probin/model/composition/dirichlet.py
+++ src/probin/model/composition/dirichlet.py
@@ -2,10 +2,22 @@
 
 from numpy import log
 import numpy as np
+from scipy.special import gammaln
 
 def fit_nonzero_parameters(sig_l,kmer_hash_count):
     pass
 
-def log_probability(sig, prob_vector):
-    pass
+def log_probability(sig,alpha):
+    return log_density(sig,alpha)
+
+def log_density(sig, alpha):
+    A = sum(alpha)
+    N = sum(sig)
+    D = len(sig)
+    return gammaln(A) - gammaln(A+N) \
+        + sum([gammaln(sig[j]+alpha[j]) - gammaln(alpha[j]) for \
+                   j in xrange(D)])
+
+def log_density_l(sig_l,alpha):
+    return sum([log_probability(sig,alpha) for sig in sig_l])
 
--- tests/test_dirichlet.py
+++ tests/test_dirichlet.py
@@ -6,6 +6,7 @@ from nose.tools import assert_almost_equal, assert_equal
 from Bio import SeqIO
 from collections import Counter
 import numpy as np
+from math import log
 
 class TestDirichlet(object):
     ## "Constants"
@@ -23,10 +24,24 @@ class TestDirichlet(object):
         # Produce strictly positive parameters
         assert_equal((distribution > 0).all,True)
 
+    def test_log_probability_without_fit(self):
+        # based on the values of the beta
+        # binomial distribution found on
+        # http://en.wikipedia.org/wiki/File:Beta-binomial_distribution_pmf.png
+        sig = [6,4]
+        alpha = [600,400]
+        p = model.log_probability(sig,alpha)
+        # bin(10,6) = 210
+        assert_almost_equal(p+log(210),log(0.249578),places=5)
+        sig = [2,8]
+        alpha = [0.2,0.25]
+        p = model.log_probability(sig,alpha)
+        # bin(10,8) = 45
+        assert_almost_equal(p+log(45), log(0.048616),places=5)
 
     def test_log_probability_order(self):
         f = fileinput.input("generated_contigs_test.fna")
-        contigs = list(SeqIO.parse(f,"fasta"))
+        c = list(SeqIO.parse(f,"fasta"))
         f.close()
         dna_c1g1 = dna.DNA(id = c[0].id, seq = str(c[0].seq))
         dna_c2g1 = dna.DNA(id = c[1].id, seq = str(c[1].seq))
@@ -41,17 +56,17 @@ class TestDirichlet(object):
         for contig in cluster1 + cluster2:
             contig.calculate_signature()
 
-        parameters1 = model.fit_nonzero_parameters([c.signature() for c in cluster1])
-        parameters2 = model.fit_nonzero_parameters([c.signature() for c in cluster2])
+        parameters1 = model.fit_nonzero_parameters([c.signature for c in cluster1])
+        parameters2 = model.fit_nonzero_parameters([c.signature for c in cluster2])
 
-        s = dna_c1g1.signature()
+        s = dna_c1g1.signature
         log_prob1 = model.log_probability(s,parameters1)
         log_prob2 = model.log_probability(s,parameters2)
         assert_equal(log_prob1>log_prob2,True)
     
-    def test_log_probability_basic(self):
+    def test_log_probability_full(self):
         f = fileinput.input("generated_contigs_test.fna")
-        contigs = list(SeqIO.parse(f,"fasta"))
+        c = list(SeqIO.parse(f,"fasta"))
         f.close()
         dna_c1g1 = dna.DNA(id = c[0].id, seq = str(c[0].seq))
         dna_c2g1 = dna.DNA(id = c[1].id, seq = str(c[1].seq))
@@ -66,16 +81,16 @@ class TestDirichlet(object):
         for contig in cluster1 + cluster2:
             contig.calculate_signature()
 
-        parameters1 = model.fit_nonzero_parameters([c.signature() for c in cluster1])
-        parameters2 = model.fit_nonzero_parameters([c.signature() for c in cluster2])
+        parameters1 = model.fit_nonzero_parameters([c.signature for c in cluster1])
+        parameters2 = model.fit_nonzero_parameters([c.signature for c in cluster2])
 
-        s1 = dna_c1g1.signature()
+        s1 = dna_c1g1.signature
         log_prob1 = model.log_probability(s1,parameters1)
         assert_almost_equal(log_prob1, -1000)
         log_prob2 = model.log_probability(s1,parameters2)
         assert_equal(log_prob2,-1000)
 
-        s2 = dna_c1g2.signature()
+        s2 = dna_c1g2.signature
         log_prob3 = model.log_probability(s2,parameters1)
         assert_equal(log_prob3,-1000)
         log_prob4 = model.log_probability(s2,parameters2)
