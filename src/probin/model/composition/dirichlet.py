ef704b943917a0ec5da83536b535cebc76071796
Johannes Alneberg <alneberg@kth.se>
2013-03-20 15:03:41 +0100

    Probability, not density

--- src/probin/model/composition/dirichlet.py
+++ src/probin/model/composition/dirichlet.py
@@ -8,13 +8,10 @@ from scipy.optimize import fmin_l_bfgs_b
 def fit_nonzero_parameters(dna_l,kmer_hash_count):
     alpha0 = [1.0]*kmer_hash_count
     alpha_bounds = [(0.0,None)]*kmer_hash_count
-    alpha_fit = fmin_l_bfgs_b(neg_log_density_l,alpha0,args=(dna_l,),maxfun=10,bounds=alpha_bounds, approx_grad=True)
+    alpha_fit = fmin_l_bfgs_b(neg_log_probability_l,alpha0,args=(dna_l,),maxfun=10,bounds=alpha_bounds, approx_grad=True)
     return alpha_fit[0]
 
 def log_probability(seq,alpha):
-    return log_density(seq,alpha)
-
-def log_density(seq,alpha):
     A = sum(alpha)
     N = sum(seq.pseudo_counts)
     D = len(alpha)
@@ -22,6 +19,6 @@ def log_density(seq,alpha):
         + sum([gammaln(seq.pseudo_count(j)+alpha[j]) \
                    - gammaln(alpha[j]) for j in xrange(D)])
 
-def neg_log_density_l(alpha,dna_l):
-    return -sum([log_density(dna,alpha) for dna in dna_l])
+def neg_log_probability_l(alpha,dna_l):
+    return -sum([log_probability(dna,alpha) for dna in dna_l])
 
