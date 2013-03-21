5ead1a881a0fa06b590d56e456f61b992078f3f5
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-03-22 00:46:53 +0100

    Added clustering and printing to ProBin

--- src/ProBin.py
+++ src/ProBin.py
@@ -4,21 +4,33 @@
 correlation between many samples."""
 import fileinput
 import sys
+import os
 from argparse import ArgumentParser
 
 from Bio import SeqIO
 
 from probin.dna import DNA
 
-def main(model,contigs,verbose):
+def main(contigs,model,clustering,verbose):
     uniform_prob = {}
     for i in xrange(DNA.kmer_hash_count):
         uniform_prob[i]= 1.0/float(DNA.kmer_hash_count)
-    for contig in contigs:
-        log_probability = model.log_probability(contig.signature,uniform_prob)
-        print log_probability
+    (clust_prob, centroids, clusters) = clustering.cluster(contigs, model, cluster_count=7 ,centroids=None, max_iter=100, repeat=10)
+    
+    return (clust_prob,centroids,clusters)
 
 
+def print_clustering_result(cluster_evaluation, centroids, clusters, arguments):
+    RESULT="""#Clustering based on parameters: {args}.
+#clustering evaluation: {clust_prob}
+#<Centroids>
+#{centroids}
+{clusters}"""
+    c = [">Cluster {0}{1}{2}".format(i,os.linesep, os.linesep.join(map(str,cluster))) for i,cluster in enumerate(clusters)]
+    params =   {"args":arguments, "clust_prob":cluster_evaluation,\
+                "clusters":os.linesep.join(c),
+                "centroids":" ".join(map(str,centroids[i]))}
+    print RESULT.format(**params)
 
 if __name__=="__main__":
     parser = ArgumentParser(description="Clustering of metagenomic contigs")
@@ -32,13 +44,22 @@ if __name__=="__main__":
         help='specify the length of kmer to use, default 4')
     parser.add_argument('-mc', '--model_composition', default='multinomial', type=str, choices=['multinomial'],
         help='specify the composition model to use, default multinomial.')
+    parser.add_argument('-c', '--clustering', default='kmeans', type=str, choices=['kmeans'],
+        help='specify the clustering to use, default kmeans.')
     args = parser.parse_args()
     
     try:
         model = __import__("probin.model.composition.{0}".format(args.model_composition),globals(),locals(),["*"],-1)
     except ImportError:
         print "Failed to load module {0}. Will now exit".format(args.model_composition)
-
+        sys.exit(-1)
+        
+    try:
+        clustering = __import__("probin.binning.{0}".format(args.clustering),globals(),locals(),["*"],-1)
+    except ImportError:
+        print "Failed to load module {0}. Will now exit".format(args.clustering)
+        sys.exit(-1)
+        
     if args.output and args.output != '-':
         sys.stdout = open(args.output, 'w')
     
@@ -48,13 +69,15 @@ if __name__=="__main__":
     DNA.generate_kmer_hash(args.kmer)
     try:
         handle = fileinput.input(args.files)
-        contigs = [DNA(x.id, x.seq.tostring().upper()) for x in list(SeqIO.parse(handle,"fasta"))]
+        contigs = [DNA(x.id, x.seq.tostring().upper(),calc_sign=True) for x in list(SeqIO.parse(handle,"fasta"))]
     except IOError as error:
         print >> sys.stderr, "Error reading file %s, message: %s" % (error.filename,error.message)
+        sys.exit(-1)
     finally:
         handle.close()
 
     if args.verbose:
         print >> sys.stderr, "parameters: %s" %(args)
     
-    main(model,contigs, args.verbose)
+    (clust_prob,centroids,clusters) = main(contigs,model,clustering, args.verbose)
+    print_clustering_result(clust_prob,centroids,clusters,args)
