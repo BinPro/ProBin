02f65d4ea9216247d4886f6b574fe7a44a86a47e
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-03-22 08:16:24 +0100

    simpler test

--- tests/test_k-means_kmer_6_contig_10000.py
+++ tests/test_k-means_kmer_6_contig_10000.py
@@ -7,11 +7,10 @@ from probin.model.composition import multinomial
 import numpy as np
 import sys
 
-class TestKmeans(object):
+class TestKmeans10000(object):
     FASTA="tests/generated_contigs_10000_test.fna"
     def setUp(self):
         dna.DNA.generate_kmer_hash(6)
-        print>>sys.stderr,dna.DNA.kmer_hash_count
         self.cluster_count = 7
         with open(self.FASTA,"r") as fh:
             seqs = SeqIO.parse(fh,"fasta")
@@ -56,8 +55,8 @@ class TestKmeans(object):
         reload(dna)
         self.contigs = []
 
-    def test_generate_kplusplus_centroids(self):
-
+    def test_generate_kplusplus_centroids10000(self):
+        
         centroids = kmeans._generate_kplusplus(self.contigs,multinomial,self.cluster_count,dna.DNA.kmer_hash_count)
         assert_equal(len(centroids), self.cluster_count)
         assert_equal(len(centroids[0]),dna.DNA.kmer_hash_count )
@@ -65,46 +64,3 @@ class TestKmeans(object):
         
         (clust_prob,new_centroids,clusters) = kmeans.cluster(self.contigs,multinomial,self.cluster_count,max_iter=100,repeat=10)
         
-        assert_equal(kmeans._evaluate_clustering(self.correct_centroids,self.correct_clusters,multinomial),clust_prob)
-
-                        
-        
-#    def test_cluster_perfect_center(self):
-#        print>>sys.stderr,"1,perfect_center"
-#
-#        correct_clusters = kmeans._expectation(self.contigs,multinomial,self.correct_centroids)
-#        print>>sys.stderr,"2,perfect_center"
-#        (clust_prob,new_centroids,clusters) = kmeans.cluster(self.contigs,multinomial,self.cluster_count,self.correct_centroids,max_iter=5,repeat=2)
-#        print>>sys.stderr,"3,perfect_center"
-#        for c in clusters:
-#            print>>sys.stderr,c
-#        print "-"*70
-#        assert_equal(kmeans._evaluate_clustering(self.correct_centroids,correct_clusters,multinomial),clust_prob)
-#
-#    def test_cluster_semi_center(self):
-#        print>>sys.stderr,"1,semi_cenroids"        
-#
-#        correct_clusters = kmeans._expectation(self.contigs,multinomial,self.correct_centroids)
-#    
-#        semi_correct_centroids = np.zeros((self.cluster_count,dna.DNA.kmer_hash_count))
-#        
-##        semi_correct_centroids[0,:] = multinomial.fit_nonzero_parameters(self.contigs[0].signature, dna.DNA.kmer_hash_count)
-##        semi_correct_centroids[1,:] = multinomial.fit_nonzero_parameters(self.contigs[9].signature, dna.DNA.kmer_hash_count)
-##        semi_correct_centroids[2,:] = multinomial.fit_nonzero_parameters(self.contigs[19].signature, dna.DNA.kmer_hash_count)
-#
-#        semi_correct_centroids[0,:] = multinomial.fit_nonzero_parameters(self.contigs[0].signature, dna.DNA.kmer_hash_count)
-#        semi_correct_centroids[1,:] = multinomial.fit_nonzero_parameters(self.contigs[3].signature,dna.DNA.kmer_hash_count)
-#        semi_correct_centroids[2,:] = multinomial.fit_nonzero_parameters(self.contigs[6].signature,dna.DNA.kmer_hash_count)
-#        semi_correct_centroids[3,:] = multinomial.fit_nonzero_parameters(self.contigs[9].signature,dna.DNA.kmer_hash_count)
-#        semi_correct_centroids[4,:] = multinomial.fit_nonzero_parameters(self.contigs[14].signature,dna.DNA.kmer_hash_count)
-#        semi_correct_centroids[5,:] = multinomial.fit_nonzero_parameters(self.contigs[19].signature,dna.DNA.kmer_hash_count)
-#        semi_correct_centroids[6,:] = multinomial.fit_nonzero_parameters(self.contigs[24].signature,dna.DNA.kmer_hash_count)
-#
-#        (clust_prob,new_centroids,clusters) = kmeans.cluster(self.contigs,multinomial,self.cluster_count,semi_correct_centroids,max_iter=5,repeat=2)
-#        print>>sys.stderr,"3,semi_cenroids"        
-#        for c in clusters:
-#            print>>sys.stderr,c
-#        print "-"*70
-#        assert_equal(kmeans._evaluate_clustering(self.correct_centroids,correct_clusters,multinomial),clust_prob)
-#        
-#        
