748c0a7535cd2de496762f403c53f9ee722dd1af
Johannes Alneberg <alneberg@kth.se>
2013-04-09 14:16:01 +0200

    Started with first coverage model

---
+++ src/probin/model/coverage/binomial.py
@@ -0,0 +1,14 @@
+"""Implementation of a binomial model based on abundance variation."""
+
+from numpy import log
+import numpy as np
+from scipy.special import gammaln
+
+def fit_nonzero_parameters(dna_l):
+    pass
+
+def neg_log_probability_l(alpha,dna_l):
+    pass
+
+def log_probability(seq,alpha):
+    pass
