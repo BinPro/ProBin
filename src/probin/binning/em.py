99680ac6ad669776e559a265b5e212ac4c4013f9
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-06-04 11:46:25 +0200

    send untransposed z to multinomial which will transpose it

--- src/probin/binning/em.py
+++ src/probin/binning/em.py
@@ -107,7 +107,7 @@ def _maximization(contigs, fit_nonzero_parameters_func, z):
     We are calculating the expression p_{k,j} = sum_i(<z_{i,k}>*theta_{i,j}) / sum_j( sum_i(<z_{i,k}>*theta_{i,j}))
     
     """
-    return fit_nonzero_parameters_func(contigs,expected_clustering=z.T)
+    return fit_nonzero_parameters_func(contigs,expected_clustering=z)
     
 
 def _evaluate_clustering(contigs, log_probabilities_func, p, z):
